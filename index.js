d350ab42d0e6ea48dc91d9ff7c9e5e5bd736aaa2
Ali Ashber <77687615+asshber@users.noreply.github.com>
2026-05-07 11:53:23 +0500

    Change response to JSON format with environment info

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.json({
+    message: 'Hello World!',
+    env: process.env
+  })
 })
 
 app.listen(3000, function () {
